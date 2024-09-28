319204b794394daac1506e636bc36ea2aa2c8d5a
Irina Klepova <ivklepova23@gmail.com>
2024-09-28 13:11:45 +0400

    добавлена маршрутизация для модели пользователя

--- users/urls.py
+++ users/urls.py
@@ -8,8 +8,15 @@ from users.views import UserCreateAPIView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
-    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token-refresh'),
-
-    path("register/", UserCreateAPIView.as_view(), name='register'),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token-refresh",
+    ),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
 ]
