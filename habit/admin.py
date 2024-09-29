e1b2a6a0ea3f3dce4d72a090570b60d82482e5ee
Irina Klepova <ivklepova23@gmail.com>
2024-09-30 01:01:11 +0400

    добавлена админка в приложение habit

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from habit.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "action",
+        "time",
+        "date",
+        "place",
+        "is_pleasant",
+        "related_habit",
+        "periodicity",
+        "complete_time",
+        "is_published",
+    )
